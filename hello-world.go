e13dad5612a2ce4baeac483de2d21574559d870b
sweeneyb <tab126@gmail.com>
2021-02-28 04:57:45 +0000

    finding a car works

--- hello-world.go
+++ hello-world.go
@@ -46,11 +46,51 @@ func printList(list *node) {
     fmt.Println(item.next.data)
     item = item.next
   }
+}
 
+func retrieve(ticket int, alleyA *node, alleyB *node) (*node, *node) {
+  var car *node = alleyA
+  if(alleyA == nil) {
+    return nil, nil
+  }
+  for alleyA.data != ticket && alleyA.next != nil {
+    alleyA, car = remove(alleyA)
+    alleyB = insert(alleyB, car.data)
+    fmt.Println("A")
+    printList(alleyA)
+    fmt.Println("B")
+    printList(alleyB)
+  }
+  if( alleyA.data == ticket) {
+    fmt.Println("Found ", ticket)
+    fmt.Println("A")
+    printList(alleyA)
+    fmt.Println("B")
+    printList(alleyB)
+    // var putBack *node
+    // alleyB, putBack = remove(alleyA)
+    alleyA, _ = remove(alleyA)
+    fmt.Println("B")
+    printList(alleyB)
+    for alleyB != nil {
+      var putBack *node
+      alleyB, putBack = remove(alleyB)
+      alleyA = insert(alleyA, putBack.data)
+    }
+    printList(alleyA)
+    return alleyA, car
+  } else {
+    for alleyB != nil {
+      alleyB, car = remove(alleyB)
+      alleyA = insert(alleyA, car.data)
+    }
+    return alleyA, nil
+  }
 }
 
 func main() {
   var alleyA *node
+  var alleyB *node
 
 	fmt.Println("Select one of the following choices:")
 	fmt.Println("1. d or D (To display the alley contents)")
@@ -70,7 +110,17 @@ func main() {
       alleyA = insert(alleyA, ticket)
 		case 3:
       fmt.Println("case 3")
-    case 5:
+      fmt.Println("Which ticket number?")
+      var ticket int
+      fmt.Scanf("%d", &ticket)
+      var node *node
+      alleyA, node = retrieve(ticket, alleyA, alleyB)
+      if(node == nil) {
+        fmt.Println("ticket not found", ticket)
+      } else {
+        fmt.Println("removed node with ticket: ", node.data)
+      }
+    case 5: // this is a remove call
       var node *node
       alleyA, node = remove(alleyA)
       if(node == nil) {
